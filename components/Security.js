e82180439addc7023363ac34679322e343caf3aa
santitroconis <santitroconis@gmail.com>
2024-11-01 08:52:19 -0400

    Security component

--- components/Security.js
+++ components/Security.js
@@ -3,7 +3,30 @@ class Security {
     this.permissionMethod = new Map();
   }
 
-  loadPermissions() {}
+  loadPermissions() {
+    db.query(/*QUERY PARA TRAER LOS PERMISOS */)
+      .then((res) => {
+        if (res.rows.length === 0) {
+          console.log("No se encontraron permisos.");
+        }
+        res.rows.forEach((element) => {
+          let key =
+            element.profile_id +
+            "_" +
+            element.method_na +
+            "_" +
+            element.object_na;
+          console.log("Cargando permiso para la clave:", key);
+
+          // Guardando el permiso en el Map
+          this.permission.set(key, true);
+        });
+        console.log(this.permission);
+      })
+      .catch((error) => {
+        console.error("Error cargando permisos:", error);
+      });
+  }
 
   hasPermission(req) {
     let key = `${req.session.profile_id}_${req.body.objectName}_${req.body.methodName}`;
