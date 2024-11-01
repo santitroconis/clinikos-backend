7812a441e06e9a71d16cbb6ea6911dd7955051c9
davidpaz06 <davidalbertopaz@gmail.com>
2024-11-01 01:49:51 -0400

    avance en register

--- components/Database.js
+++ components/Database.js
@@ -19,11 +19,35 @@ class Database {
   async query(queryName, params) {
     const client = await this.pool.connect();
     try {
-      const query = this.queries[queryName] || queryName,
-        res = await client.query(query, params);
+      const query = this.queries[queryName] || queryName;
+      res = await client.query(query, params);
       return res;
     } catch (err) {
       console.error(`Error executing query:`, err);
+      throw err;
+    } finally {
+      client.release();
+    }
+  }
+
+  async transaction(queryArray, paramsArray) {
+    const client = await this.pool.connect();
+    try {
+      await client.query("BEGIN");
+      let results = [];
+      for (let i = 0; i < queryArray.length; i++) {
+        const query = this.queries[queryArray[i]] || queryArray[i];
+        const params = paramsArray[i];
+        console.log(`Ejecutando consulta: ${query}`);
+        console.log(`Con parámetros: ${JSON.stringify(params)}`);
+        const res = await client.query(query, params);
+        results.push(res);
+      }
+      await client.query("COMMIT");
+      return results;
+    } catch (err) {
+      await client.query("ROLLBACK");
+      console.error(`Error executing transaction:`, err);
       throw err;
     } finally {
       client.release();
